087fdfcaebfce3845eba212ff5169ebd8e302d36
Thomas Mack <email@bielefeldts.de>
2018-12-19 14:27:33 +0100

    Minesweeper Board implemented

--- minesweeper/src/minesweeper.js
+++ minesweeper/src/minesweeper.js
@@ -18,3 +18,27 @@ console.log('This is what a board with a guess and a bomb on it would look like.
 console.log(guessLine);
 console.log(bombLine);
 console.log(blankLine);
+
+
+let board = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']];
+console.log(board);
+
+const printBoard = (board) => {
+console.log('Current Board:');
+
+  for (i = 0;i<board.length;i++) {
+    var row = board[i].join(' | ');
+    //for (j=0;j<board[i].length; j++) {
+
+      //row = row +board[i][j];
+      //if(j<board[i].length -1) row = row+'|';
+    //}
+    console.log(row);
+  }
+}
+printBoard(board);
+
+board[0][1] = 1;
+board[2][2] = 'B';
+
+printBoard(board);
