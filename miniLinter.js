ee539835d91dfaa06c5e3c6664cce40a05438347
Tommy <tommy@bidat-mac-pro.local>
2018-12-21 07:04:22 +0100

    miniLinter Project CodeCademy

---
+++ miniLinter.js
@@ -0,0 +1,34 @@
+let story = 'Last weekend, I took literally the most beautiful bike ride of my life. The route is called "The 9W to Nyack" and it actually stretches all the way from Riverside Park in Manhattan to South Nyack, New Jersey. It\'s really an adventure from beginning to end! It is a 48 mile loop and it basically took me an entire day. I stopped at Riverbank State Park to take some extremely artsy photos. It was a short stop, though, because I had a really long way left to go. After a quick photo op at the very popular Little Red Lighthouse, I began my trek across the George Washington Bridge into New Jersey.  The GW is actually very long - 4,760 feet! I was already very tired by the time I got to the other side.  An hour later, I reached Greenbrook Nature Sanctuary, an extremely beautiful park along the coast of the Hudson.  Something that was very surprising to me was that near the end of the route you actually cross back into New York! At this point, you are very close to the end.';
+
+let overusedWords = ['really', 'very', 'basically'];
+
+let unnecessaryWords = ['extremely', 'literally', 'actually' ];
+
+let storyWords = story.split(' ');
+console.log("wordcount: "+storyWords.length);
+
+storyWords = storyWords.filter(word => {
+  //console.log(word+"->"+unnecessaryWords.indexOf(word));
+  if(unnecessaryWords.indexOf(word)===-1) return word;
+})
+let outputArray = [];
+storyWords = storyWords.forEach(word => {
+ // console.log(word+"->"+(overusedWords.indexOf(word)>=0));
+  if((overusedWords.indexOf(word)>=0)) outputArray.push(word.toUpperCase()) ;
+  else outputArray.push(word);
+})
+console.log("wordcount: "+outputArray.length);
+console.log(outputArray.join(' '));
+
+const getSentenceCount = (arrayToCount) => {
+  var count = 0;
+  var chars = ['.','!'];
+  for(let i=0;i<chars.length;i++) {
+    var newArray = arrayToCount.split(chars[i]);
+    count += newArray.length;
+  }
+  return count;
+}
+
+console.log('Sentence count: '+getSentenceCount(story));
+
